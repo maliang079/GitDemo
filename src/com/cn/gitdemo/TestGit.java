95f364adfdc8edb337cc634c263adec8d0ce4dfb
maliang079 <maliang079@126.com>
2014-05-13 17:38:51 +0800

    no message

--- src/com/cn/gitdemo/TestGit.java
+++ src/com/cn/gitdemo/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 		System.err.println("aaa");
+		System.err.println("ccc");
     }
 
 }
