ae8f9f5a999d52bd11c7580bd9e8ef52b55e3f23
maliang079 <maliang079@126.com>
2014-05-13 17:33:52 +0800

    mal upd

--- src/com/cn/gitdemo/TestGit.java
+++ src/com/cn/gitdemo/TestGit.java
@@ -3,8 +3,7 @@ package com.cn.gitdemo;
 public class TestGit {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.err.println("aaa");
     }
 
 }
