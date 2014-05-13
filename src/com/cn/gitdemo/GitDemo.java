09e02fbd74e378d80c9e1e6bd292fadc1287b59d
maliang079 <maliang079@126.com>
2014-05-13 17:20:04 +0800

    mal upd

--- src/com/cn/gitdemo/GitDemo.java
+++ src/com/cn/gitdemo/GitDemo.java
@@ -5,4 +5,9 @@ public class GitDemo {
     public String sayHelloWorld() {
         return "mal say hello world.";
     }
+
+    public String sayHw() {
+        return "my is sayHw mothd.";
+    }
+
 }
