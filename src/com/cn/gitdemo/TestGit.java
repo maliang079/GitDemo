849dc15669e7385c75981d34f923ea8e683ed3f0
maliang079 <maliang079@126.com>
2014-05-14 09:48:46 +0800

    111

--- src/com/cn/gitdemo/TestGit.java
+++ src/com/cn/gitdemo/TestGit.java
@@ -3,11 +3,7 @@ package com.cn.gitdemo;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.err.println("aaa");
-        System.err.println("bbb");
-        System.err.println("ccc");
-		System.err.println("ddd");
-		System.err.println("fff");
+        System.err.println("111");
     }
 
 }
